695721d246e07ac59e070ab65f81848468c2071e
Suleman pervez <119485959+Suleman1411@users.noreply.github.com>
2023-10-24 05:44:50 +0500

    10/24/2023

--- index.js
+++ index.js
@@ -1,74 +1,114 @@
 // import inquirer from "inquirer";
-// let todos: string[] = [];
+// const todos: string[] = [];
 // let loop = true;
-// while(loop){
-//     const answers:{
-//         TODO: string,
-//         addmore: boolean
-//     }= await inquirer.prompt([{
-//         type:"input",
-//         name:"TODO",
-//         message: "What do you want to add in your todo?",
-//     },
-// {
-//     type:"confrim",
-//     name:"addmore",
-//     message:"do you want to add more todo?(y/n)",
-//     default: false,
-// }])
-// const {TODO, addmore} = answers;
-// console.log(answers)
-// loop = addmore
-// if(TODO) {
-//     todos.push(TODO)
-// } else {
-//     console.log("kindly add valid input")
+// async function main() {
+//   while (loop) {
+//     const answers = await inquirer.prompt([
+//       {
+//         type: "input",
+//         name: "TODO",
+//         message: "What do you want to add to your todo?",
+//       },
+//       {
+//         type: "confirm",
+//         name: "addmore",
+//         message: "Do you want to add more todos? (y/n)",
+//         default: false,
+//       },
+//     ]);
+//     const { TODO, addmore } = answers;
+//     console.log(answers);
+//     if (TODO) {
+//       todos.push(TODO);
+//     } else {
+//       console.log("Kindly add a valid input.");
+//     }
+//     loop = addmore;
+//   }
+//   if (todos.length > 0) {
+//     console.log("Your Todos List:\n");
+//     todos.forEach((todo) => {
+//       console.log(todo);
+//     });
+//   } else {
+//     console.log("No todos found.");
+//   }
 // }
-// }
-// if(todos.length>0) {
-//     console.log("Your Todos List: \n")
-//  todos.forEach(todo => {
-//     console.log(todo)
-//  });
-// } else {
-//     console.log("No todos found")
-// }
+// main();
 import inquirer from "inquirer";
 const todos = [];
 let loop = true;
 async function main() {
     while (loop) {
-        const answers = await inquirer.prompt([
+        const actionAnswers = await inquirer.prompt([
             {
-                type: "input",
-                name: "TODO",
-                message: "What do you want to add to your todo?",
-            },
-            {
-                type: "confirm",
-                name: "addmore",
-                message: "Do you want to add more todos? (y/n)",
-                default: false,
+                type: "list",
+                name: "action",
+                message: "Choose an action:",
+                choices: ["Add Task", "Edit Task", "View Tasks", "Quit"],
             },
         ]);
-        const { TODO, addmore } = answers;
-        console.log(answers);
-        if (TODO) {
-            todos.push(TODO);
+        const { action } = actionAnswers;
+        if (action === "Add Task") {
+            const addTaskAnswers = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "TODO",
+                    message: "What do you want to add to your todo?",
+                },
+            ]);
+            const { TODO } = addTaskAnswers;
+            if (TODO) {
+                todos.push(TODO);
+                console.log(`Task "${TODO}" added to your todo list.`);
+            }
+            else {
+                console.log("Kindly add a valid input.");
+            }
         }
-        else {
-            console.log("Kindly add a valid input.");
+        else if (action === "Edit Task") {
+            if (todos.length === 0) {
+                console.log("No tasks to edit. Add tasks first.");
+            }
+            else {
+                const editTaskAnswers = await inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "taskIndex",
+                        message: "Select a task to edit:",
+                        choices: todos,
+                    },
+                    {
+                        type: "input",
+                        name: "newTask",
+                        message: "Enter the new task description:",
+                    },
+                ]);
+                const { taskIndex, newTask } = editTaskAnswers;
+                const index = todos.indexOf(taskIndex);
+                if (index !== -1) {
+                    todos[index] = newTask;
+                    console.log(`Task "${taskIndex}" updated to "${newTask}".`);
+                }
+                else {
+                    console.log("Task not found in the list.");
+                }
+            }
         }
-        loop = addmore;
-    }
-    if (todos.length > 0) {
-        console.log("Your Todos List:\n");
-        todos.forEach((todo) => {
-            console.log(todo);
-        });
-    }
-    else {
-        console.log("No todos found.");
+        else if (action === "View Tasks") {
+            if (todos.length > 0) {
+                console.log("Your Todos List:\n");
+                todos.forEach((todo) => {
+                    console.log(todo);
+                });
+            }
+            else {
+                console.log("No tasks found.");
+            }
+        }
+        else if (action === "Quit") {
+            loop = false;
+        }
     }
 }
 main();
